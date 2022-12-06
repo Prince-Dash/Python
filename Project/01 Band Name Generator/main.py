522553ec33f45787c925862a9e5171f7908ad723
A15 TUF Prince <princepritamdash@gmail.com>
2022-12-06 15:06:35 +0530

    Brand Name Generator Project Added

---
+++ Project/01 Band Name Generator/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Brand Name Generator.")
+city = input("What's the name of the city you grew up in?\t")
+pet_name = input("What's your pet's name?\t")
+print("Your Band Name could be", city, pet_name)
